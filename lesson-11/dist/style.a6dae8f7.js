6d378fb2d729015f0b45cd7a28e2c7b4741074f7
amonchik8 <amonchik8@gmail.com>
2020-10-22 15:15:18 +0300

    add card__inner

--- lesson-11/dist/style.a6dae8f7.js
+++ lesson-11/dist/style.a6dae8f7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"D:\\git\\beetroot\\lesson-11\\src\\images\\kiwi1.png":[["kiwi1.4e729339.png","images/kiwi1.png"],"images/kiwi1.png"],"_css_loader":"C:/Users/amon/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/amon/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"C:/Users/amon/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"C:/Users/amon/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53345" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59932" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
