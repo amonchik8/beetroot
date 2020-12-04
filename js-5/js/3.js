036cb336fd0be7672127731163258ca1b9e377f2
amonchik8 <amonchik8@gmail.com>
2020-12-04 23:59:56 +0200

    add while

--- js-5/js/3.js
+++ js-5/js/3.js
@@ -16,72 +16,80 @@ function changeSec(obj, value) {
         obj.hour += (value / 3600);
     } else if (value % 60 === 0) {
         obj.min += (value / 60);
-
-        // if (value > 59) {
-        //     convertMin(value)
-        //     obj.hour += 1;
-        // }
-
-        if (obj.min > 59) {
+        if (obj.min < 10) {
+            obj.min = `0${obj.min}`;
+        };
+        while (obj.min > 59) {
+            obj.min -= 60;
             obj.hour += 1;
-            obj.min = obj.min - 60;
             if (obj.min < 10) {
                 obj.min = `0${obj.min}`;
-            };  
-        };
-        
-        
+            };
+        };     
     } else {
         obj.min += Math.floor(value / 60);
-        if (obj.min > 59) {
+        while (obj.min > 59) {
+            obj.min -= 60;
             obj.hour += 1;
-            obj.min = obj.min - 60;
-            if (obj.min < 10) {
-                obj.min = `0${obj.min}`;
-            };  
+            // if (obj.min < 10) {
+            //     obj.min = `0${obj.min}`;
+            // };
         };
+        
         value = value % 60;
         obj.sec += value;
-        if (obj.sec > 59) {
-            obj.min += 1;
-            obj.sec = obj.sec - 60;
-            if (obj.sec < 10) {
-                obj.sec = `0${obj.sec}`;
-            };
+    };
+    while (obj.sec > 59) {
+        obj.min += 1;
+        if (obj.min < 10) {
+            obj.min = `0${obj.min}`;
+        };
+        obj.sec = obj.sec - 60;
+        if (obj.sec < 10) {
+            obj.sec = `0${obj.sec}`;
+        };
+    };
+    while (obj.min > 59) {
+        obj.hour += 1;
+        obj.min -= 60;
+        if (obj.min < 10) {
+            obj.min = `0${obj.min}`;
         }; 
     };
-    if (obj.hour > 23) {
+    while (obj.hour > 23) {
         obj.hour = obj.hour - 24;
         if (obj.hour < 10) {
             obj.hour = `0${obj.hour}`;
         };  
     };
-    if (obj.min > 59) {
-        obj.hour += 1;
-        obj.min = obj.min - 60;
-        if (obj.min < 10) {
-            obj.min = `0${obj.min}`;
-        };  
-    };
-    if (obj.min > 59) {
-        obj.hour += 1;
-        obj.min = obj.min - 60;
-        if (obj.min < 10) {
-            obj.min = `0${obj.min}`;
-        };  
-    };
+    
+    
+    // if (obj.min > 59) {
+    //     obj.hour += 1;
+    //     obj.min = obj.min - 60;
+    //     if (obj.min < 10) {
+    //         obj.min = `0${obj.min}`;
+    //     };  
+    // };
     console.log(`${obj.hour}:${obj.min}:${obj.sec}`);
 };
 
 
 
-changeSec(time, 6800);
-// 11100
+changeSec(time, 4000 );
 
-function convertMin(value) {
-    while (value > 59) {
-        value -= 60;
-        convertMin(value);
-    } 
-    return value;
-}
+
+
+
+// function changeHour(obj, value) {
+//     obj.hour += value;
+//     if (obj.hour > 23) {
+//         obj.hour = obj.hour - 24;
+//         if (obj.hour < 10) {
+//             obj.hour = `0${obj.hour}`;
+//         };  
+//     };
+//     console.log(`${obj.hour}:${obj.min}:${obj.sec}`);
+// };
+
+// changeHour(time, 28);
