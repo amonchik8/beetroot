45c47c8ae1737bf412039d5a069bef161ae0595f
amonchik8 <amonchik8@gmail.com>
2020-12-08 22:11:28 +0200

    add task-3

---
+++ js-6/js/3.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const styles = [
+    {
+        name : "color:",
+        meaning : "red;",
+    },
+    {
+        name : "font-size:",
+        meaning : "20px;",
+    },
+    {
+        name : "text-align:",
+        meaning : "center;",
+    },
+];
+
+function getText(arr, text) {
+    document.write(`<p style ="${styles[0].name}${styles[0].meaning}${styles[1].name}${styles[1].meaning}${styles[2].name}${styles[2].meaning}">${text}</p>`);
+    
+};
+
+getText(styles, "Текст");
