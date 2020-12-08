5f23e2f358beebbb3d35d4b4e7bedef9965857b6
amonchik8 <amonchik8@gmail.com>
2020-12-08 16:36:30 +0200

    add task-2

--- js-6/js/1.js
+++ js-6/js/1.js
@@ -19,50 +19,41 @@ const shoppingList = [
 ];
 
 /////1
-// function needBuy() {
-//   shoppingList.sort(function (a, b) {
-//     if (a.buy > b.buy) {
-//       return 1;
-//     };
-//     if (a.buy < b.buy) {
-//       return -1;
-//     };
-//     return 0;
-//   });
-//   for (let i = 0; i < shoppingList.length; i++) {
-//     alert(`${shoppingList[i].name}`);
-//   };
-// };
-// needBuy();
+function needBuy() {
+  shoppingList.sort(function (a, b) {
+    if (a.buy > b.buy) {
+      return 1;
+    };
+    if (a.buy < b.buy) {
+      return -1;
+    };
+    return 0;
+  });
+  for (let i = 0; i < shoppingList.length; i++) {
+    alert(`${shoppingList[i].name}`);
+  };
+};
 
 /////2
-// let buy = {
-//   name: "meet",
-//   amount: 1,
-//   buy: false,
-// };
+let buy = {
+  name: "meet",
+  amount: 1,
+  buy: false,
+};
 
-// function addBuy(obj) {
-//   const len = shoppingList.length;
-//   let hasMatch = false;
-//   for (let i = 0; i < len; i++) {
-//     if (buy.name === shoppingList[i].name) {
-//       shoppingList[i].amount += 1;
-//       hasMatch = true;
-//     };
-//   };
-//   if (!hasMatch) shoppingList.push(buy);
-// };
-
-// addBuy(buy);
-// console.log(shoppingList);
+function addBuy(obj) {
+  const len = shoppingList.length;
+  let hasMatch = false;
+  for (let i = 0; i < len; i++) {
+    if (buy.name === shoppingList[i].name) {
+      shoppingList[i].amount += 1;
+      hasMatch = true;
+    };
+  };
+  if (!hasMatch) shoppingList.push(buy);
+};
 
 /////3
-let buy = {
-    name: "milk",
-    amount: 1,
-    buy: false,
-  };
 function toBuy(name) {
   const len = shoppingList.length;
   for (let i = 0; i < len; i++) {
@@ -71,6 +62,3 @@ function toBuy(name) {
     };
   };    
 };
-
-toBuy(buy.name);
-console.log(shoppingList);
--- js-6/js/2.js
+++ js-6/js/2.js
@@ -8,16 +8,17 @@ let check = [
   },
   {
     name: "beer",
-    amount: 1,
-    price: 40,
+    amount: 2,
+    price: 80,
   },
   {
     name: "cheese",
-    amount: 1,
-    price: 60,
+    amount: 3,
+    price: 30,
   },
 ];
 
+/////1
 function printOnScreen() {
   let i = 0;
   let arr = [];
@@ -28,4 +29,33 @@ function printOnScreen() {
   alert(str);
 };
 
-printOnScreen();
+/////2
+function sumBuy(arr) {
+  let price = arr.map((item) => item.price * item.amount);
+  let sum = 0;
+  price.forEach(function (item, index, array) {
+    sum += item;
+  });
+  return sum;
+};
+
+/////3
+function getMostExpensiveBuy(arr) {
+  arr.sort((a, b) => b.price * b.amount - a.price * a.amount);
+  alert(`${arr[0].name}, ${arr[0].amount}, ${arr[0].price}`);
+};
+
+/////4
+function sumAmout(arr) {
+  let amoutArr = arr.map((item) => item.amount);
+  let amout = 0;
+  amoutArr.forEach(function (item, index, array) {
+    amout += item;
+  });
+  return amout;
+};
+
+function averageCost(arr) {
+  let avr = sumBuy(arr) / sumAmout(arr);
+  return avr;
+};
