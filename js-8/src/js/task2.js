3d3384cc529fe5947960fd5f1a07dafd4c9a0109
amonchik8 <amonchik8@gmail.com>
2020-12-14 22:50:07 +0200

    add task2

---
+++ js-8/src/js/task2.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const $button = document.getElementById("button");
+const $popup = document.getElementById("popup");
+const $popupCloseButton = document.querySelector(".popup button");
+
+const onPopupClose = () => {
+  $popup.classList.remove("_active");
+};
+
+const showPopup = () => {
+  $popup.classList.add("_active");
+};
+
+const hidePopup = () => {
+  $popup.classList.remove("_active");
+};
+
+$button.addEventListener("click", () => {
+  showPopup();
+});
+
+$popupCloseButton.addEventListener("click", onPopupClose);
