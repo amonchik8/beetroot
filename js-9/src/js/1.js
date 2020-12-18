6a7c892be355251d73d2bd4e879ee1ea42f97c2a
amonchik8 <amonchik8@gmail.com>
2020-12-19 00:34:22 +0200

    add task2

--- js-9/src/js/1.js
+++ js-9/src/js/1.js
@@ -2,15 +2,18 @@
 
 const $textDiv = document.querySelector(".textdiv");
 const $textArea = document.querySelector(".textarea");
+const $discription = document.querySelector(".textdiv__description");
 
 const showTextArea = () => {
   $textArea.classList.add("_visible");
   $textDiv.classList.add("_hide");
+  $discription.innerHTML = '<= Edit me and press ctrl + ,'
 };
 
 const hideTextArea = () => {
   $textArea.classList.remove("_visible");
   $textDiv.classList.remove("_hide");
+  $discription.innerHTML = 'Press ctrl + E'
 };
 
 document.addEventListener("keydown", (e) => {
